8b2e1e2566be15ecf7410be24d417c0d7133e146
judamar <juancho.o.mart@gmail.com>
2025-07-04 01:18:02 -0500

    getfullbox

--- carpetmodSrc/carpet/commands/CommandGetFullBox.java
+++ carpetmodSrc/carpet/commands/CommandGetFullBox.java
@@ -21,7 +21,7 @@ public class CommandGetFullBox extends CommandCarpetBase {
 
     @Override
     public String getUsage(ICommandSender sender) {
-        return "/getfullbox <item> [sb color]";
+        return "/getfullbox <item> [meta] [sb-color]";
     }
 
     @Override
@@ -29,34 +29,44 @@ public class CommandGetFullBox extends CommandCarpetBase {
         if (!(sender instanceof EntityPlayerMP))
             throw new CommandException("Only players can use this command");
 
-        if (args.length < 1 || args.length > 2)
+        if (args.length < 1 || args.length > 3)
             throw new WrongUsageException(getUsage(sender));
 
         EntityPlayerMP player = (EntityPlayerMP) sender;
 
-        // 1. Obtener ítem
         String itemId = args[0];
         Item item = Item.REGISTRY.getObject(new ResourceLocation(itemId));
         if (item == null)
             throw new CommandException("Unknown item: " + itemId);
 
-        // 2. Obtener color (si aplica)
+        int meta = 0;
         EnumDyeColor color = null;
-        if (args.length == 2) {
+
+        if (args.length >= 2) {
             try {
-                color = EnumDyeColor.valueOf(args[1].toUpperCase());
-            } catch (IllegalArgumentException e) {
-                throw new CommandException("Invalid color: " + args[1]);
+                meta = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                try {
+                    color = EnumDyeColor.valueOf(args[1].toUpperCase());
+                } catch (IllegalArgumentException ex) {
+                    throw new CommandException("Invalid metadata or color: " + args[1]);
+                }
             }
         }
 
-        // 3. Obtener ítem de shulker box
+        if (args.length == 3) {
+            try {
+                color = EnumDyeColor.valueOf(args[2].toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new CommandException("Invalid color: " + args[2]);
+            }
+        }
+
         Block shulkerBlock = (color == null)
                 ? Blocks.PURPLE_SHULKER_BOX
                 : BlockShulkerBox.getBlockByColor(color);
         Item shulkerItem = Item.getItemFromBlock(shulkerBlock);
 
-        // 4. Crear shulker llena con el ítem
         ItemStack shulkerStack = new ItemStack(shulkerItem);
         NBTTagCompound shulkerTag = new NBTTagCompound();
         NBTTagCompound blockEntityTag = new NBTTagCompound();
@@ -68,6 +78,7 @@ public class CommandGetFullBox extends CommandCarpetBase {
             tag.setByte("Slot", (byte) i);
             tag.setString("id", Item.REGISTRY.getNameForObject(item).toString());
             tag.setByte("Count", (byte) maxStackSize);
+            tag.setShort("Damage", (short) meta);
             items.appendTag(tag);
         }
 
@@ -75,7 +86,6 @@ public class CommandGetFullBox extends CommandCarpetBase {
         shulkerTag.setTag("BlockEntityTag", blockEntityTag);
         shulkerStack.setTagCompound(shulkerTag);
 
-        // 5. Entregar al jugador
         boolean added = player.inventory.addItemStackToInventory(shulkerStack);
         if (!added)
             throw new CommandException("No space in inventory to add shulker box");
