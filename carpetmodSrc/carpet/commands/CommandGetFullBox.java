ad0adbe35f95e6db6b82890488bf7e0324506a43
judamar <juancho.o.mart@gmail.com>
2025-07-12 00:37:59 -0500

    feat(commands): add /getfullbox

--- carpetmodSrc/carpet/commands/CommandGetFullBox.java
+++ carpetmodSrc/carpet/commands/CommandGetFullBox.java
@@ -1,3 +1,4 @@
+//ported from: https://github.com/CommandLeo/scarpet/blob/main/programs/getfullbox.sc
 package carpet.commands;
 
 import carpet.utils.Messenger;
@@ -26,71 +27,77 @@ public class CommandGetFullBox extends CommandCarpetBase {
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        if (!(sender instanceof EntityPlayerMP))
-            throw new CommandException("Only players can use this command");
+        if (!command_enabled("commandGetFullBox", sender)) {
+            if (!(sender instanceof EntityPlayerMP))
+                throw new CommandException("Only players can use this command");
 
-        if (args.length < 1 || args.length > 3)
-            throw new WrongUsageException(getUsage(sender));
+            if (args.length < 1 || args.length > 3)
+                throw new WrongUsageException(getUsage(sender));
 
-        EntityPlayerMP player = (EntityPlayerMP) sender;
+            EntityPlayerMP player = (EntityPlayerMP) sender;
 
-        String itemId = args[0];
-        Item item = Item.REGISTRY.getObject(new ResourceLocation(itemId));
-        if (item == null)
-            throw new CommandException("Unknown item: " + itemId);
+            if (!player.interactionManager.getGameType().isCreative()) {
+                throw new CommandException("You must be in Creative mode to use this command.");
+            }
 
-        int meta = 0;
-        EnumDyeColor color = null;
+            String itemId = args[0];
+            Item item = Item.REGISTRY.getObject(new ResourceLocation(itemId));
+            if (item == null)
+                throw new CommandException("Unknown item: " + itemId);
 
-        if (args.length >= 2) {
-            try {
-                meta = Integer.parseInt(args[1]);
-            } catch (NumberFormatException e) {
+            int meta = 0;
+            EnumDyeColor color = null;
+
+            if (args.length >= 2) {
                 try {
-                    color = EnumDyeColor.valueOf(args[1].toUpperCase());
-                } catch (IllegalArgumentException ex) {
-                    throw new CommandException("Invalid metadata or color: " + args[1]);
+                    meta = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    try {
+                        color = EnumDyeColor.valueOf(args[1].toUpperCase());
+                    } catch (IllegalArgumentException ex) {
+                        throw new CommandException("Invalid metadata or color: " + args[1]);
+                    }
                 }
             }
-        }
 
-        if (args.length == 3) {
-            try {
-                color = EnumDyeColor.valueOf(args[2].toUpperCase());
-            } catch (IllegalArgumentException e) {
-                throw new CommandException("Invalid color: " + args[2]);
+            if (args.length == 3) {
+                try {
+                    color = EnumDyeColor.valueOf(args[2].toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    throw new CommandException("Invalid color: " + args[2]);
+                }
             }
+
+            Block shulkerBlock = (color == null)
+                    ? Blocks.PURPLE_SHULKER_BOX
+                    : BlockShulkerBox.getBlockByColor(color);
+            Item shulkerItem = Item.getItemFromBlock(shulkerBlock);
+
+            ItemStack shulkerStack = new ItemStack(shulkerItem);
+            NBTTagCompound shulkerTag = new NBTTagCompound();
+            NBTTagCompound blockEntityTag = new NBTTagCompound();
+            NBTTagList items = new NBTTagList();
+
+            int maxStackSize = item.getItemStackLimit();
+            for (int i = 0; i < 27; i++) {
+                NBTTagCompound tag = new NBTTagCompound();
+                tag.setByte("Slot", (byte) i);
+                tag.setString("id", Item.REGISTRY.getNameForObject(item).toString());
+                tag.setByte("Count", (byte) maxStackSize);
+                tag.setShort("Damage", (short) meta);
+                items.appendTag(tag);
+            }
+
+            blockEntityTag.setTag("Items", items);
+            shulkerTag.setTag("BlockEntityTag", blockEntityTag);
+            shulkerStack.setTagCompound(shulkerTag);
+
+            boolean added = player.inventory.addItemStackToInventory(shulkerStack);
+            if (!added)
+                throw new CommandException("No space in inventory to add shulker box");
+
+            Messenger.m(player, "g Gave a full shulker box of ", "w " + itemId, "g  with color ", "w " + (color == null ? "purple" : color.getName()));
         }
-
-        Block shulkerBlock = (color == null)
-                ? Blocks.PURPLE_SHULKER_BOX
-                : BlockShulkerBox.getBlockByColor(color);
-        Item shulkerItem = Item.getItemFromBlock(shulkerBlock);
-
-        ItemStack shulkerStack = new ItemStack(shulkerItem);
-        NBTTagCompound shulkerTag = new NBTTagCompound();
-        NBTTagCompound blockEntityTag = new NBTTagCompound();
-        NBTTagList items = new NBTTagList();
-
-        int maxStackSize = item.getItemStackLimit();
-        for (int i = 0; i < 27; i++) {
-            NBTTagCompound tag = new NBTTagCompound();
-            tag.setByte("Slot", (byte) i);
-            tag.setString("id", Item.REGISTRY.getNameForObject(item).toString());
-            tag.setByte("Count", (byte) maxStackSize);
-            tag.setShort("Damage", (short) meta);
-            items.appendTag(tag);
-        }
-
-        blockEntityTag.setTag("Items", items);
-        shulkerTag.setTag("BlockEntityTag", blockEntityTag);
-        shulkerStack.setTagCompound(shulkerTag);
-
-        boolean added = player.inventory.addItemStackToInventory(shulkerStack);
-        if (!added)
-            throw new CommandException("No space in inventory to add shulker box");
-
-        Messenger.m(player, "g Gave a full shulker box of ", "w " + itemId, "g  with color ", "w " + (color == null ? "purple" : color.getName()));
     }
 
     @Override
